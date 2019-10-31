22c74c4212229d9f9c01f14d1434fc8cc88723aa
System Administrator <trido1903@gmail.com>
2019-10-30 23:18:26 -0700

    Updates

---
+++ precache-manifest.8fc86febfa5922fb509c18e76bc528f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59178938424c3b4c393d2c0126821ce4",
+    "url": "/hangman-app/index.html"
+  },
+  {
+    "revision": "0bcedbf9648942bbefc7",
+    "url": "/hangman-app/static/css/main.7897dd53.chunk.css"
+  },
+  {
+    "revision": "8026f68b2cd1b54c748d",
+    "url": "/hangman-app/static/js/2.e872fd29.chunk.js"
+  },
+  {
+    "revision": "0bcedbf9648942bbefc7",
+    "url": "/hangman-app/static/js/main.46634bf9.chunk.js"
+  },
+  {
+    "revision": "f8cc3125380e9e5e2ae4",
+    "url": "/hangman-app/static/js/runtime-main.6c0f93f4.js"
+  }
+]);
